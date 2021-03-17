92e51e2e4396c4eb1affd149609b8827cea5b6b9
jpapazian2000 <jpapazian@hashicorp.com>
2021-03-17 11:31:22 +0100

    commit variables change

--- variables.tf
+++ variables.tf
@@ -28,35 +28,6 @@ variable "az_vm_size" {
     description = "size of the vm to deploy"
     default     = "Standard_B1s"
 }
-variable "az_image_publisher" {
-  description = "Name of the publisher of the image (az vm image list)"
-  default     = "Canonical"
-}
-
-variable "az_image_offer" {
-  description = "Name of the offer (az vm image list)"
-  default     = "UbuntuServer"
-}
-
-variable "az_image_sku" {
-  description = "Image SKU to apply (az vm image list)"
-  default     = "18.04-LTS"
-}
-
-variable "az_image_version" {
-  description = "Version of the image to apply (az vm image list)"
-  default     = "latest"
-}
-
-variable "az_computer_name" {
-    type        = string
-    description = "os name for the azure vm" 
-}
-
-variable "az_admin_password" {
-    type        = string
-    description = "admin password of the centos az vm"
-}
 
 variable "az_image_data_disk_size" {
   description = "size of the data disk image in gb"
