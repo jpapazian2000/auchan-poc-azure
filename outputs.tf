63f7b9375eb8da430d4aa1ca76d1f501563131c0
jpapazian2000 <jpapazian@hashicorp.com>
2021-03-17 18:28:20 +0100

    modified syntax outputs

--- outputs.tf
+++ outputs.tf
@@ -2,12 +2,12 @@
 output "az_public_ip" {
   //value =  module.jpapazian_az_vm.m_az_public_ip
   value = {
-    for p in  keys(locals.az_zones) : p => module.jpapazian_az_vm[p].m_az_public_ip
+    for p in  keys(local.az_zones) : p => module.jpapazian_az_vm[p].m_az_public_ip
   }
 } 
 output "az_private_ip_address" {
     //value = module.jpapazian_az_vm.m_az_private_ip_address
     value = {
-    for p in  keys(locals.az_zones) : p => module.jpapazian_az_vm[p].m_az_private_ip_address
+    for p in  keys(local.az_zones) : p => module.jpapazian_az_vm[p].m_az_private_ip_address
   }
 }
