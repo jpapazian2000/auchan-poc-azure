f3a12063a100fac3506d1f5fd8161ee939da7808
jpapazian2000 <jpapazian@hashicorp.com>
2021-03-17 18:50:39 +0100

    changed outputs def

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,17 @@
 
 output "az_public_ip" {
   //value =  module.jpapazian_az_vm.m_az_public_ip
-  value = {
+  /*value = {
     for p in  keys(local.az_zones) : p => module.jpapazian_az_vm[p].m_az_public_ip
   }
+  */
+  value = zipmap( values(module.jpapazian_az_vm)[*].name,values(module.jpapazian_az_vm)[*].m_az_public_ip )
 } 
-output "az_private_ip_address" {
+//output "az_private_ip_address" {
     //value = module.jpapazian_az_vm.m_az_private_ip_address
-    value = {
+    /*value = {
     for p in  keys(local.az_zones) : p => module.jpapazian_az_vm[p].m_az_private_ip_address
   }
-}
+  */
+
+//}
