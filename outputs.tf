929df94de4f1a998ee566628c80613f4a662cf8d
jpapazian2000 <jpapazian@hashicorp.com>
2021-03-17 17:52:13 +0100

    change

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 
 output "az_public_ip" {
-  value =  [ module.jpapazian_az_vm.*.m_az_public_ip ]
+  value =  [ module.jpapazian_az_vm.m_az_public_ip.* ]
 } 
 output "az_private_ip_address" {
-    value = [ module.jpapazian_az_vm.*.m_az_private_ip_address ]
+    value = [ module.jpapazian_az_vm.m_az_private_ip_address.* ]
 }
