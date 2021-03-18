cadeafd35468f7ee0c2c78582ae62c2f7df7b8d7
jpapazian2000 <jpapazian@hashicorp.com>
2021-03-18 18:46:04 +0100

    changed address prefix to conditional expression

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ data "azurerm_image" "auchanImage" {
 module "jpapazian_az_vm" {
     
   source  = "app.terraform.io/jpapazian-org/jpapazian-az-vm/azurerm"
-  version = "0.1.2"
+  version = "0.1.3"
     for_each                    = local.az_zones
     m_az_zone                   = each.value
     m_az_vm_size                = var.az_vm_size
