c0bcb533ca38d2a37d12534011ef4e679aeaee0e
jpapazian2000 <jpapazian@hashicorp.com>
2021-03-19 13:46:12 +0100

    2 zones deployement

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 
-//locals {
-//    az_zones = toset([ "1", "2"])
-//}
+locals {
+    az_zones = toset([ "1", "2"])
+}
 
 data "azurerm_resource_group" "project_rg" {
     name                    = "jpapazian-${var.az_project}"
@@ -17,9 +17,9 @@ module "jpapazian_az_vm" {
     
   source  = "app.terraform.io/jpapazian-org/jpapazian-az-vm/azurerm"
   version = "0.1.5"
-    //for_each                    = local.az_zones
-    //m_az_zone                   = each.value
-    m_az_zone                   = "1"
+    for_each                    = local.az_zones
+    m_az_zone                   = each.value
+    //m_az_zone                   = "1"
     m_az_vm_size                = var.az_vm_size
     m_az_project                = var.az_project
     m_az_location               = var.az_location
